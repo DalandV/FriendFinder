ba254985141e0736bd4f9a6e24dc5d9d5c5d6d14
Daland Dixon <dalanddixon@Dalands-MBP.fios-router.home>
2018-10-30 13:55:26 -0400

    Files and Folders Setup

---
+++ app/routing/htmlRoutes.js
@@ -0,0 +1,20 @@
+// DEPENDENCIES
+// =============================================================================
+var path = require("path");
+
+// ROUTER
+// =============================================================================
+module.exports = function (app) {
+    // HTML GET Requests
+    // Below code handles when users "visit" a page.
+    // In each of the below cases the user is shown an HTML page of content
+    // ---------------------------------------------------------------------------
+
+    app.get("*", function (req, res) {
+        res.sendFile(path.join(__dirname, "../public/home.html"));
+    });
+
+    app.get("/surveys", function (req, res) {
+        res.sendFile(path.join(__dirname, "../public/survey.html"));
+    });
+};
