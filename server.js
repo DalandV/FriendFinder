3e7ef771e8165693711fd49cb7d71d11788ad9d5
Daland Dixon <dalanddixon@Dalands-MBP.fios-router.home>
2019-02-01 09:36:56 -0500

    Links on the bottom of the page open in new tab

--- server.js
+++ server.js
@@ -8,13 +8,16 @@ var app = express();
 
 var PORT = process.env.PORT || 8080;
 
+app.use(express.static('app/public'));
+
+// DATA PARSING
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// ROUTER
+// ROUTES
 // =============================================================================
-require("./app/routing/apiRoutes")(app);
-require("./app/routing/htmlRoutes")(app);
+require("./app/routes/apiRoutes")(app);
+require("./app/routes/htmlRoutes")(app);
 
 // LISTENER
 // =============================================================================
