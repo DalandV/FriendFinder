589d20f2489b312e8f4da1714c6224681a786eb7
Daland Dixon <dalanddixon@Dalands-MBP.fios-router.home>
2019-02-02 16:01:31 -0500

    Modal works

--- app/public/survey/survey.js
+++ app/public/survey/survey.js
@@ -1,20 +1,20 @@
-$(document).ready(function () {
-  console.log("It Worked!")
-  $(".submit").on("click", function (event) {
+$(document).ready(function() {
+  $(".submit").on("click", function(event) {
     event.preventDefault();
-    console.log($("#item-1").val());
-    var name = $('#name-input').val().trim();
-    var photo = $('#photo-input').val();
-    var item1 = parseInt($('#item-1').val());
-    var item2 = parseInt($('#item-2').val());
-    var item3 = parseInt($('#item-3').val());
-    var item4 = parseInt($('#item-4').val());
-    var item5 = parseInt($('#item-5').val());
-    var item6 = parseInt($('#item-6').val());
-    var item7 = parseInt($('#item-7').val());
-    var item8 = parseInt($('#item-8').val());
-    var item9 = parseInt($('#item-9').val());
-    var item10 = parseInt($('#item-10').val());
+    var name = $("#name-input")
+      .val()
+      .trim();
+    var photo = $("#photo-input").val();
+    var item1 = parseInt($("#item-1").val());
+    var item2 = parseInt($("#item-2").val());
+    var item3 = parseInt($("#item-3").val());
+    var item4 = parseInt($("#item-4").val());
+    var item5 = parseInt($("#item-5").val());
+    var item6 = parseInt($("#item-6").val());
+    var item7 = parseInt($("#item-7").val());
+    var item8 = parseInt($("#item-8").val());
+    var item9 = parseInt($("#item-9").val());
+    var item10 = parseInt($("#item-10").val());
 
     var userInput = {
       name: name,
@@ -31,23 +31,25 @@ $(document).ready(function () {
         item9,
         item10
       ]
-    }
+    };
 
-    $.post("/api/friends", userInput, function (data) {
+    $.post("/api/friends", userInput, function(data) {
       console.log(data);
+
+      $(".modal").modal("toggle");
       // Clear the form when submitting
-      $('#name-input').val("")
-      $('#photo-input').val("");
-      $('#item-1').val(0);
-      $('#item-2').val(0);
-      $('#item-3').val(0);
-      $('#item-4').val(0);
-      $('#item-5').val(0);
-      $('#item-6').val(0);
-      $('#item-7').val(0);
-      $('#item-8').val(0);
-      $('#item-9').val(0);
-      $('#item-10').val(0);
+      $("#name-input").val("");
+      $("#photo-input").val("");
+      $("#item-1").val(0);
+      $("#item-2").val(0);
+      $("#item-3").val(0);
+      $("#item-4").val(0);
+      $("#item-5").val(0);
+      $("#item-6").val(0);
+      $("#item-7").val(0);
+      $("#item-8").val(0);
+      $("#item-9").val(0);
+      $("#item-10").val(0);
     });
   });
 });
--- app/routes/apiRoutes.js
+++ app/routes/apiRoutes.js
@@ -1,11 +1,11 @@
 var friendsData = require("../data/friends");
 
-module.exports = function (app) {
-    app.get("/api/friends", function (req, res) {
-        res.send(friendsData);
-    });
-    app.post("/api/friends", function (req, res) {
-        friendsData.push(req.body);
-        res.json(true);
-    });
+module.exports = function(app) {
+  app.get("/api/friends", function(req, res) {
+    res.send(friendsData);
+  });
+  app.post("/api/friends", function(req, res) {
+    friendsData.push(req.body);
+    res.json(true);
+  });
 };
