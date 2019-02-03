ae28b38205f04da9cc88bc61d2686865841270f7
Daland Dixon <dalanddixon@Dalands-MBP.fios-router.home>
2019-02-03 09:38:58 -0500

    Added more default friends in friends.js

--- app/data/friends.js
+++ app/data/friends.js
@@ -4,6 +4,18 @@ var friendsArray = [
     photo:
       "https://image.shutterstock.com/image-photo/portrait-happy-older-man-wearing-260nw-73143091.jpg",
     scores: [1, 4, 2, 3, 5, 1, 3, 2, 4, 1]
+  },
+  {
+    name: "Angry Doggo",
+    photo:
+      "https://cdn.psychologytoday.com/sites/default/files/styles/article-inline-half/public/field_blog_entry_images/2018-02/vicious_dog_0.png?itok=nsghKOHs",
+    scores: [2, 3, 2, 4, 1, 5, 2, 3, 4, 2]
+  },
+  {
+    name: "Shaggy",
+    photo:
+      "https://cdn.vox-cdn.com/thumbor/a7NRXKgvr4o7853XSju7rJQn3UA=/0x0:1280x720/1200x800/filters:focal(538x258:742x462)/cdn.vox-cdn.com/uploads/chorus_image/image/62953072/shaggyyy.0.jpg",
+    scores: [3, 2, 5, 2, 1, 3, 4, 5, 2, 1]
   }
 ];
 
