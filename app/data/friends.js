3e37b686bef0a1c3030910498315fe9eff24ca79
Daland Dixon <dalanddixon@Dalands-MBP.fios-router.home>
2019-02-03 09:56:35 -0500

    Changed photo link for default friend

--- app/data/friends.js
+++ app/data/friends.js
@@ -14,7 +14,7 @@ var friendsArray = [
   {
     name: "Shaggy",
     photo:
-      "https://cdn.vox-cdn.com/thumbor/a7NRXKgvr4o7853XSju7rJQn3UA=/0x0:1280x720/1200x800/filters:focal(538x258:742x462)/cdn.vox-cdn.com/uploads/chorus_image/image/62953072/shaggyyy.0.jpg",
+      "https://cdn.vox-cdn.com/uploads/chorus_image/image/62953072/shaggyyy.0.jpg",
     scores: [3, 2, 5, 2, 1, 3, 4, 5, 2, 1]
   }
 ];
